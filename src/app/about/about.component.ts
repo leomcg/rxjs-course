d29404d87b58f9d37c8e864f7d5f0ce98afb1ba6
Leonardo Gonçalves <leomcg@gmail.com>
2020-09-17 09:24:20 -0300

    map operator

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Observable, interval, timer, fromEvent, noop } from 'rxjs';
+import { timeInterval } from 'rxjs/operators';
+import { createHttpObservable } from '../common/util';
 
 @Component({
   selector: 'about',
@@ -10,6 +13,17 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    
+    const http$ = createHttpObservable('/api/courses');
+
+    http$.subscribe(
+      courses => console.log(courses),
+      noop,
+      () => console.log('completed')
+    );
+
+    
+
   }
 
 }
